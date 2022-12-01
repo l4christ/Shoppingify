b319a571c2eea2436f7345c34eae5b33f7633c7c
Sirloko <lekswyde205@gmail.com>
2022-12-01 17:16:52 +0100

    added items model

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function show(item $item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(item $item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, item $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(item $item)
+    {
+        //
+    }
+}
