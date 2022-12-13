a2cadc7586352be0c0f6a8704f5ff82578a499ee
Sirloko <lekswyde205@gmail.com>
2022-12-13 13:26:18 +0100

    route update

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ItemController;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
