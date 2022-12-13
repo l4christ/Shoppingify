5136b5ae62fcbc58768b4c90eba44bc4c1ef0b75
Sirloko <lekswyde205@gmail.com>
2022-12-13 11:49:37 +0100

    updated routes

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::get('/item', [ItemController::class, 'fetchItem']);
